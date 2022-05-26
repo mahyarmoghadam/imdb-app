d745827b30ad4c2ed4761df020482f2b38acb882
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-27 02:37:06 +0430

    fixed : scrollable home content

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,18 +1,21 @@
-import { Block } from 'expo-ui-kit';
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import HeaderComponent from './components/HeaderComponent';
+import { Block } from "expo-ui-kit";
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import BodyComponent from "./components/BodyComponent";
+import HeaderComponent from "./components/HeaderComponent";
 
 export default function HomeScreen() {
-    return (
-        <Block style={styles.container}>
-            <HeaderComponent/>
-        </Block>
-    );
+  return (
+    <Block scroll style={styles.container}>
+      <HeaderComponent />
+
+      <BodyComponent />
+    </Block>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#140f14'
-    }
+  container: {
+    backgroundColor: "#140f14",
+  },
 });
