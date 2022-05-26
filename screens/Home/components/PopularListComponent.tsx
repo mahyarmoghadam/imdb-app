ee83321af6201b43c4a4660e80c26d4f8cd71028
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-27 03:10:37 +0430

    feature : add popular list component

---
+++ screens/Home/components/PopularListComponent.tsx
@@ -0,0 +1,65 @@
+import { AntDesign } from "@expo/vector-icons";
+import { Block, Text } from "expo-ui-kit";
+import { FlatList } from "native-base";
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import Colors from "../../../models/constants/Colors";
+import { fakeMovieList } from "../../../models/movies/fake-movie";
+
+export default function PopularListComponent() {
+  const renderContent = ({ item }: any) => {
+    return (
+      <Block style={styles.container}>
+        <Block>
+          <Image style={styles.imageContainer} source={item.image} />
+        </Block>
+        <Block row space="between">
+          <Text style={styles.movieNameText} color={Colors.secondaryTextColor}>
+            {item.movieName}
+          </Text>
+          <Text style={styles.movieNameText} color={Colors.secondaryTextColor}>
+            {item.rate}
+            <AntDesign name="star" size={18} color="#dbb28c" />
+          </Text>
+        </Block>
+
+        <Block>
+          <Text style={styles.releaseYearText} color={Colors.basicTextColor}>
+            {item.releaseYear}
+          </Text>
+        </Block>
+      </Block>
+    );
+  };
+
+  return (
+    <FlatList
+      data={fakeMovieList}
+      renderItem={(item) => renderContent(item)}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 20,
+    paddingRight: 20,
+  },
+
+  imageContainer: {
+    width: 175,
+    height: 235,
+    borderRadius: 20,
+  },
+
+  movieNameText: {
+    fontWeight: "800",
+    paddingTop: 10,
+  },
+
+  releaseYearText: {
+    fontWeight: '600',
+  },
+});
