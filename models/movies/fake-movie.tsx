a187cfef9ee043e9cbd30356cabcd2aa281bc8f9
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-27 03:09:43 +0430

    imp : add fake db (movie list )

---
+++ models/movies/fake-movie.tsx
@@ -0,0 +1,44 @@
+const fakeMovieList = [
+  {
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/en/7/74/Shang-Chi_and_the_Legend_of_the_Ten_Rings_poster.jpeg",
+    },
+    movieName: "Shang Chi",
+    releaseYear: 2021,
+    rate: 7.8,
+  },
+  {
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
+    },
+    movieName: "Dune",
+    releaseYear: 2021,
+    rate: 8.2,
+  },
+  {
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/en/1/1c/Free_Guy_2021_Poster.jpg",
+    },
+    movieName: "Free Guy",
+    releaseYear: 2021,
+    rate: 7.1,
+  },
+  {
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/en/f/f6/Cruella_2021_film_poster.jpg",
+    },
+    movieName: "cruella",
+    releaseYear: 2021,
+    rate: 7.3
+  },
+  {
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/en/1/19/Minions_%282015_film%29.jpg",
+    },
+    movieName: "Minions",
+    releaseYear: 2015,
+    rate: 6.4,
+  },
+];
+
+export { fakeMovieList };
