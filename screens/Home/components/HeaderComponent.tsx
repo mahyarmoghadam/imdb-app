00de05895478e852c22574e6c7bd0a923db035a1
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-27 01:34:52 +0430

    imp : add header component

---
+++ screens/Home/components/HeaderComponent.tsx
@@ -0,0 +1,35 @@
+import { Block, Text } from "expo-ui-kit";
+import { StyleSheet, Image } from "react-native";
+
+export default function HeaderComponent() {
+  return (
+    <Block marginTop={20} style={styles.container} row >
+      <Block>
+        <Text size={24} weight={"600"} color={'#354259'}>
+          Hi, Emran
+        </Text>
+      </Block>
+      <Block style={styles.userPhotoContainer}>
+        <Image style={styles.userPhoto} source={{uri:'https://i.picsum.photos/id/984/200/300.jpg?hmac=mLBN3lSvSl08Vh8Kw96TLY7v239gr1idtxVXvYFDkSc'}}/>
+      </Block>
+    </Block>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+
+  userPhotoContainer:{
+    alignItems:'flex-end'
+  },
+
+  userPhoto:{
+      width:50,
+      height:50,
+      borderRadius:50
+  }
+});
--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,28 +1,16 @@
 import { Block } from 'expo-ui-kit';
+import React from 'react';
 import { StyleSheet, Text } from 'react-native';
-
+import HeaderComponent from './components/HeaderComponent';
 
 export default function HomeScreen() {
     return (
-        <Block>
-            <Text>Hello</Text>
+        <Block >
+            <HeaderComponent/>
         </Block>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+    
 });
