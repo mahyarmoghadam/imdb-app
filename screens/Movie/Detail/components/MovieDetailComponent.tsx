04ab51e5a4825051b3c9526f8300dd8eeb37deae
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-27 16:02:52 +0430

    add : movie description style

--- screens/Movie/Detail/components/MovieDetailComponent.tsx
+++ screens/Movie/Detail/components/MovieDetailComponent.tsx
@@ -8,9 +8,18 @@ export default function MovieDetailComponent() {
   return (
     <Block style={styles.container}>
       <Block row>
-        <TagComponent tagName="Epic"/>
-        <TagComponent tagName="Fantasy"/>
-        <TagComponent tagName="Drama"/>
+        <TagComponent tagName="Epic" />
+        <TagComponent tagName="Fantasy" />
+        <TagComponent tagName="Drama" />
+      </Block>
+
+      <Block paddingHorizontal={5} paddingVertical={15}>
+        <Text weight={'600'} color={Colors.secondaryTextColor} size={12}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni alias
+          quae obcaecati voluptas facilis quia sint explicabo. Illum, eum
+          necessitatibus. Hic odit optio pariatur vero quia aspernatur vel
+          magnam laudantium... <Text color={Colors.basicTextColor} size={12}>more</Text>
+        </Text>
       </Block>
     </Block>
   );
@@ -19,7 +28,7 @@ export default function MovieDetailComponent() {
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 15,
-    paddingVertical:5,
+    paddingVertical: 5,
     backgroundColor: "#201921",
   },
 });
