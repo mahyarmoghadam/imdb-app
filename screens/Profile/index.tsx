4e80a7a735ec00aa4cf79b6b7120e2b2c863f39d
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-28 22:15:59 +0430

    fixed: profile body component in profile screen

--- screens/Profile/index.tsx
+++ screens/Profile/index.tsx
@@ -1,12 +1,14 @@
 import { Block } from "expo-ui-kit";
 import React from "react";
-import { StyleSheet } from "react-native";
+import ProfileBodyComponent from "./components/ProfileBody/ProfileBodyComponent";
 import ProfileHeaderComponent from "./components/ProfileHeader/ProfileHeaderComponent";
 
 export default function ProfileScreen() {
   return (
-    <Block scroll color='#140f14'>
+    <Block scroll color="#140f14">
       <ProfileHeaderComponent />
+
+      <ProfileBodyComponent />
     </Block>
   );
 }
