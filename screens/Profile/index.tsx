8f9ca41fcd18bdfa9e47cddeb99192a772489b5a
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-05-28 20:04:42 +0430

    add : add profile screen

--- screens/Profile/index.tsx
+++ screens/Profile/index.tsx
@@ -1,13 +1,16 @@
 import { Block } from "expo-ui-kit";
 import React from "react";
 import { StyleSheet } from "react-native";
+import ProfileHeaderComponent from "./components/ProfileHeader";
 
 export default function ProfileScreen() {
-  return <Block scroll style={styles.container}></Block>;
+  return (
+    <Block scroll color='#140f14'>
+      <ProfileHeaderComponent />
+    </Block>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#140f14",
-  },
+
 });
