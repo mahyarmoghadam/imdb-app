4054815d53d311e40f90beeb987b3f9b2ec0869b
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-06-05 21:29:41 +0430

    add: explore screen

---
+++ screens/Movie/Explore/index.tsx
@@ -0,0 +1,10 @@
+import { Block } from "expo-ui-kit";
+import React from "react";
+
+export default function ExploreScreen() {
+  return (
+    <Block scroll color='#201921' >
+        
+    </Block>
+  );
+}
