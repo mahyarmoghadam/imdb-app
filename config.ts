e8ee8b8845c715a63716a8b41b80897937a4f58f
emraan.jadidi <emraanjadidi2@gmail.com>
2022-06-29 16:08:06 +0430

    add : image helper

--- config.ts
+++ config.ts
@@ -1,15 +1,19 @@
 const _API_KEY = '2a12ec511a2f51bac0c7a0c701e4d3f2';
 const _TMDB_API = 'https://api.themoviedb.org/3';
-const _TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
+const _TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/';
 
 
 const generateUrl = (path?: string) => {
-    if(path?.includes('?')){
+    if (path?.includes('?')) {
         return `${_TMDB_API}/${path}&api_key=${_API_KEY}`
     }
-    else{
+    else {
         return `${_TMDB_API}/${path}?api_key=${_API_KEY}`
     }
 }
 
-export { generateUrl };
+const getImageUrl = (imagePath: string, width: string = 'w500') => {
+    return `${_TMDB_IMAGE_URL}/${width}/${imagePath}`;
+}
+
+export { generateUrl, getImageUrl };
