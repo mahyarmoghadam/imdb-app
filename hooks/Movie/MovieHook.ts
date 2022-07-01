af0d318cb732f243936a73f2a6087a915552e02d
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-07-01 11:33:01 +0430

    refactor : better coding

--- hooks/Movie/MovieHook.ts
+++ hooks/Movie/MovieHook.ts
@@ -4,22 +4,15 @@ import { Get } from "@generalService"
 import { MovieList } from "@models";
 
 const useMovieList = (movieId: number, page: number = 1, lang: string = 'en-US') => {
-
-    const result = useQuery(QueryKeys.MovieList, async () => {
+    return useQuery(QueryKeys.MovieList, async () => {
         return await Get<MovieList>(`movie/${movieId}/lists?language=${lang}&page=${page}`);
     });
-
-    return result;
 };
 
-
 const useMovieDetail = (movieId: number, lang: string = 'en-US') => {
-
-    const result = useQuery(QueryKeys.MovieDetail, async () => {
+     return useQuery(QueryKeys.MovieDetail, async () => {
         return await Get<MovieList>(`movie/${movieId}?language=${lang}`);
     });
-
-    return result;
 }
 
 export { useMovieList, useMovieDetail };
--- hooks/People/PeopleHook.ts
+++ hooks/People/PeopleHook.ts
@@ -3,15 +3,11 @@ import { Get } from "@generalService";
 import { PopularPeopleResult } from "@models";
 import { useQuery } from "react-query";
 
-
-
 const usePopularPeople = (movieId: number, page: number = 1, lang: string = 'en-US') => {
 
-    const result = useQuery(QueryKeys.PeoplePopular, async () => {
+    return useQuery(QueryKeys.PeoplePopular, async () => {
         return await Get<PopularPeopleResult>(`movie/${movieId}/lists?language=${lang}&page=${page}`);
     });
-
-    return result;
 };
 
 export { usePopularPeople };
