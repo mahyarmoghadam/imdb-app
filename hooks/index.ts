9ec82392e5d2c064768aabd658abec82da88e068
EmraanJadidi76 <emraanjadidi2@gmail.com>
2022-07-14 19:34:43 +0430

    config : hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,7 +1,21 @@
 import useTheme from "./useTheme";
 import useColorScheme from "./useColorScheme";
 import useCachedResources from "./useCachedResources";
-import { useMovieList, usePopular, useTopRated } from "./Movie/MovieHook";
+import {
+  useMovieDetail,
+  useMovieList,
+  usePopular,
+  useTopRated,
+} from "./Movie/MovieHook";
 import { usePopularPeople } from "./People/PeopleHook";
 
-export { useTheme, useColorScheme, useCachedResources, useMovieList, usePopularPeople, usePopular, useTopRated };
+export {
+  useTheme,
+  useColorScheme,
+  useCachedResources,
+  useMovieList,
+  usePopularPeople,
+  usePopular,
+  useTopRated,
+  useMovieDetail,
+};
